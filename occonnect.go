1cbfbb57f8f358559a98d4036804cb93a4f166e7
Tommy Ma <tma@buf.build>
2022-06-21 17:09:45 -0400

    small cleanup

--- occonnect.go
+++ occonnect.go
@@ -52,8 +52,7 @@ var (
 )
 
 var (
-	DefaultLatencyDistribution      = view.Distribution(1, 2, 3, 4, 5, 6, 8, 10, 13, 16, 20, 25, 30, 40, 50, 65, 80, 100, 130, 160, 200, 250, 300, 400, 500, 650, 800, 1000, 2000, 5000, 10000, 20000, 50000, 100000)
-	DefaultMillisecondsDistribution = view.Distribution(0.01, 0.05, 0.1, 0.3, 0.6, 0.8, 1, 2, 3, 4, 5, 6, 8, 10, 13, 16, 20, 25, 30, 40, 50, 65, 80, 100, 130, 160, 200, 250, 300, 400, 500, 650, 800, 1000, 2000, 5000, 10000, 20000, 50000, 100000)
+	DefaultLatencyDistribution      = view.Distribution(0.01, 0.05, 0.1, 0.3, 0.6, 0.8, 1, 2, 3, 4, 5, 6, 8, 10, 13, 16, 20, 25, 30, 40, 50, 65, 80, 100, 130, 160, 200, 250, 300, 400, 500, 650, 800, 1000, 2000, 5000, 10000, 20000, 50000, 100000)
 	DefaultMessageCountDistribution = view.Distribution(1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536)
 )
 
@@ -74,33 +73,33 @@ var (
 var (
 	ClientRoundtripLatencyView = &view.View{
 		Name:        "connect.build/http/client/roundtrip_latency",
-		Measure:     ochttp.ClientRoundtripLatency,
-		Aggregation: DefaultLatencyDistribution,
 		Description: "End-to-end latency, by method.",
 		TagKeys:     []tag.Key{ochttp.KeyClientPath},
+		Measure:     ochttp.ClientRoundtripLatency,
+		Aggregation: DefaultLatencyDistribution,
 	}
 
 	ClientCompletedRPCsView = &view.View{
-		Measure:     ochttp.ClientRoundtripLatency,
 		Name:        "connect.build/client/completed_rpcs",
 		Description: "Count of RPCs by method and status.",
 		TagKeys:     []tag.Key{ochttp.KeyClientPath, KeyClientStatus},
+		Measure:     ochttp.ClientRoundtripLatency,
 		Aggregation: view.Count(),
 	}
 
 	ClientSentMessagesPerRPCView = &view.View{
-		Measure:     ClientSentMessagesPerRPC,
 		Name:        "connect.build/client/sent_messages_per_rpc",
 		Description: "Distribution of sent messages count per RPC, by method.",
 		TagKeys:     []tag.Key{ochttp.KeyClientPath},
+		Measure:     ClientSentMessagesPerRPC,
 		Aggregation: DefaultMessageCountDistribution,
 	}
 
 	ClientReceivedMessagesPerRPCView = &view.View{
-		Measure:     ClientReceivedMessagesPerRPC,
 		Name:        "connect.build/client/received_messages_per_rpc",
 		Description: "Distribution of received messages count per RPC, by method.",
 		TagKeys:     []tag.Key{ochttp.KeyClientPath},
+		Measure:     ClientReceivedMessagesPerRPC,
 		Aggregation: DefaultMessageCountDistribution,
 	}
 )
@@ -113,7 +112,7 @@ var (
 		Description: "Distribution of server latency in milliseconds, by method.",
 		TagKeys:     []tag.Key{ochttp.KeyServerRoute},
 		Measure:     ochttp.ServerLatency,
-		Aggregation: DefaultMillisecondsDistribution,
+		Aggregation: DefaultLatencyDistribution,
 	}
 
 	ServerCompletedRPCsView = &view.View{
