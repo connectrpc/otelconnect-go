130741f898d2854dfda9797d3dea2c2e70a515ab
Tommy Ma <33741651+cyinma@users.noreply.github.com>
2022-06-22 17:08:36 -0400

    add status tag for unary call (#7)

--- unary_tracker.go
+++ unary_tracker.go
@@ -24,19 +24,26 @@ import (
 )
 
 func newUnaryTracker(unaryFunc connect.UnaryFunc) connect.UnaryFunc {
-	return func(ctx context.Context, request connect.AnyRequest) (connect.AnyResponse, error) {
+	return func(ctx context.Context, request connect.AnyRequest) (_ connect.AnyResponse, retErr error) { // nolint:nonamedreturns
 		ochttp.SetRoute(ctx, request.Spec().Procedure)
-		defer finishUnaryTracking(ctx, request.Spec().IsClient, request.Spec().Procedure)
+		defer func() {
+			finishUnaryTracking(ctx, request.Spec().IsClient, request.Spec().Procedure, retErr)
+		}()
 		return unaryFunc(ctx, request)
 	}
 }
 
-func finishUnaryTracking(ctx context.Context, isClient bool, procedure string) {
+func finishUnaryTracking(ctx context.Context, isClient bool, procedure string, retErr error) {
+	status := "ok"
+	if retErr != nil {
+		status = connect.CodeOf(retErr).String()
+	}
 	var tags []tag.Mutator
 	var measurements []stats.Measurement
 	if isClient {
 		tags = []tag.Mutator{
 			tag.Upsert(ochttp.KeyClientPath, procedure),
+			tag.Upsert(KeyClientStatus, status),
 		}
 		measurements = []stats.Measurement{
 			ClientSentMessagesPerRPC.M(1),
@@ -45,6 +52,7 @@ func finishUnaryTracking(ctx context.Context, isClient bool, procedure string) {
 	} else {
 		tags = []tag.Mutator{
 			tag.Upsert(ochttp.KeyServerRoute, procedure),
+			tag.Upsert(KeyServerStatus, status),
 		}
 		measurements = []stats.Measurement{
 			ServerSentMessagesPerRPC.M(1),
