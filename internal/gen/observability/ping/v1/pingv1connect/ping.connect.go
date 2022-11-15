a924e8002ac87a6a6da6e1bbe646c6c025b22781
joshcarp <jcarpeggiani@buf.build>
2022-11-15 17:51:54 -0500

    Undo change

--- internal/gen/observability/ping/v1/pingv1connect/ping.connect.go
+++ internal/gen/observability/ping/v1/pingv1connect/ping.connect.go
@@ -7,11 +7,10 @@ package pingv1connect
 import (
 	context "context"
 	errors "errors"
-	http "net/http"
-	strings "strings"
-
 	connect_go "github.com/bufbuild/connect-go"
 	v1 "github.com/bufbuild/connect-opentelemetry-go/internal/gen/observability/ping/v1"
+	http "net/http"
+	strings "strings"
 )
 
 // This is a compile-time assertion to ensure that this generated file and the connect package are
