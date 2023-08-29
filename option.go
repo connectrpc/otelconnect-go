8dac70f68187d990cc260129f1141a511e5ba83f
Philip K. Warren <pwarren@buf.build>
2023-08-29 10:25:13 -0500

    Update to latest OTEL and semconv
    Update to the latest version of the OTEL libraries. Migrate from semconv
    1.12.0 to 1.21.0 (matching upstream opentelemetry-go change). Upgrade
    build dependencies.

--- option.go
+++ option.go
@@ -22,7 +22,7 @@ import (
 	"go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/metric/noop"
 	"go.opentelemetry.io/otel/propagation"
-	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 	"go.opentelemetry.io/otel/trace"
 )
 
