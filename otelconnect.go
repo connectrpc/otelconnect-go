77538562aac285e7ec1a859812baddbea8ab7c7a
Edward McFarlane <3036610+emcfarlane@users.noreply.github.com>
2025-09-22 15:53:29 +0100

    Change version to 0.8.1-dev
    Signed-off-by: Edward McFarlane <3036610+emcfarlane@users.noreply.github.com>

--- otelconnect.go
+++ otelconnect.go
@@ -25,7 +25,7 @@ import (
 )
 
 const (
-	version             = "0.8.1"
+	version             = "0.8.1-dev"
 	semanticVersion     = "semver:" + version
 	instrumentationName = "connectrpc.com/otelconnect"
 	grpcProtocol        = "grpc"
