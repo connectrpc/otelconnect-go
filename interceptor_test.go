fe19c852cb34da8819f9b1810c8278a69cafa47c
Edward McFarlane <emcfarlane@buf.build>
2026-03-02 18:23:16 +0100

    Upgrade lint
    Signed-off-by: Edward McFarlane <emcfarlane@buf.build>

--- interceptor_test.go
+++ interceptor_test.go
@@ -2245,14 +2245,15 @@ type wantSpans struct {
 	attrs    []attribute.KeyValue
 }
 
-func assertSpans(t *testing.T, want []wantSpans, got []trace.ReadOnlySpan) {
+func assertSpans(t *testing.T, wants []wantSpans, got []trace.ReadOnlySpan) {
 	t.Helper()
-	require.Len(t, got, len(want), "unexpected spans length")
+	require.Len(t, got, len(wants), "unexpected spans length")
 	for i, span := range got {
-		wantEvents := want[i].events
-		wantAttributes := want[i].attrs
+		want := wants[i] //nolint: gosec // index bounds asserted above
+		wantEvents := want.events
+		wantAttributes := want.attrs
 		assert.False(t, span.StartTime().IsZero(), "span start time is nil")
-		assert.Equal(t, want[i].spanName, span.Name(), "unexpected span name")
+		assert.Equal(t, want.spanName, span.Name(), "unexpected span name")
 		gotEvents := span.Events()
 		require.Len(t, gotEvents, len(wantEvents), "unexpected events length")
 		for i, e := range wantEvents {
