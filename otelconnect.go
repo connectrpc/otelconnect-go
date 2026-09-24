c4a87c96b153f15f1f9c9bc0d88d5ae9116eea90
Edward McFarlane <3036610+emcfarlane@users.noreply.github.com>
2026-09-24 19:44:54 +0100

    Back to development (#219)
    Signed-off-by: Edward McFarlane <emcfarlane@buf.build>

--- otelconnect.go
+++ otelconnect.go
@@ -25,7 +25,7 @@ import (
 )
 
 const (
-	version             = "0.10.0"
+	version             = "0.11.0-dev"
 	semanticVersion     = "semver:" + version
 	instrumentationName = "connectrpc.com/otelconnect"
 
